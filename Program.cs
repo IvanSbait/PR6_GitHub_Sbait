5e60f115ffd4a1efdd0cdf9b737a861990db0796
IvanSbait <superpuper36bg+Sbait@gmail.com>
2025-04-18 13:07:42 +0300

    Implement point distance calculation and user input for coordinates

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // read 4 points from the user
+        Console.WriteLine("Enter the coordinates of the first point (x1 y1): ");
+        string[] point1 = Console.ReadLine().Split(' ');
+        double x1 = double.Parse(point1[0]);
+        double y1 = double.Parse(point1[1]);
+
+        Console.WriteLine("Enter the coordinates of the second point (x2 y2): ");
+        string[] point2 = Console.ReadLine().Split(' ');
+        double x2 = double.Parse(point2[0]);
+        double y2 = double.Parse(point2[1]);
+
+        Console.WriteLine("Enter the coordinates of the third point (x3 y3): ");
+        string[] point3 = Console.ReadLine().Split(' ');
+        double x3 = double.Parse(point3[0]);
+        double y3 = double.Parse(point3[1]);
+
+        // calculate the distance between the points
+        double distance1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        double distance2 = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+        double distance3 = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+
+
+        Console.WriteLine($"Distance between point 1 and point 2: {distance1}");
+        Console.WriteLine($"Distance between point 2 and point 3: {distance2}");
+        Console.WriteLine($"Distance between point 3 and point 1: {distance3}");
+    }
+}
--- obj/Debug/net9.0/PR6_GitHub_Sbait.AssemblyInfo.cs
+++ obj/Debug/net9.0/PR6_GitHub_Sbait.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PR6_GitHub_Sbait")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff4330680d64b75ae6f088736656a71b4662cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PR6_GitHub_Sbait")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PR6_GitHub_Sbait")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
